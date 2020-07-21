776b31a862d4eb7a990dad1337ce59966fd6595d
yhg0337 <58197935+yhg0337@users.noreply.github.com>
2020-07-21 21:33:48 +0900

    Desktop MainPage

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Visual = styled.section`
   width: 100%;
@@ -9,7 +9,6 @@ const Visual = styled.section`
   background-position: center;
   position: relative;
 `;
-
 const SearchBox = styled.div`
   display: flex;
   flex-direction: column;
@@ -24,12 +23,19 @@ const SearchBox = styled.div`
 `;
 const SearchTop = styled.ul`
   width: 880px;
-  margin: 15px auto;
+  margin: 0 auto;
+  padding: 15px 0;
 
   display: flex;
   a {
+    height: 100%;
     padding: 10px 20px;
     text-decoration: none;
+    &:hover {
+      li {
+        color: #7986cb;
+      }
+    }
     li {
       color: #fff;
       span {
@@ -77,7 +83,6 @@ const SearchBTN = styled.button`
   border: none;
   cursor: pointer;
 `;
-
 const FeatureContainer = styled.section`
   width: 880px;
   margin: 0 auto;
@@ -110,6 +115,11 @@ const Summary = styled.div`
     outline: none;
     border: 1px solid #a7c0cd;
     color: #a7c0cd;
+    cursor: pointer;
+    &:hover {
+      color: #fff;
+      background: #a7c0cd;
+    }
   }
 `;
 const Remian = styled.div`
@@ -132,6 +142,120 @@ const NewsList = styled.ul`
     &:hover {
       text-decoration: underline;
     }
+  }
+`;
+const DownAppContainer = styled.section`
+  background: rgb(246, 246, 246);
+  width: 100%;
+  height: 380px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const DownAppImg = styled.section`
+  width: 500px;
+  height: 255px;
+  background-image: url("https://s.zigbang.com/v1/web/main/img_3.jpg");
+  background-size: contain;
+  background-position: center;
+  background-repeat: no-repeat;
+  margin-right: 15px;
+`;
+const DownAppLink = styled.div`
+  width: 30%;
+  height: 50%;
+  margin: auto 0;
+  margin-left: 15px;
+  h1 {
+    font-size: 25px;
+    font-weight: bold;
+    margin: 10px 0;
+  }
+  a {
+    background-size: cover;
+    background-position: top;
+    background-repeat: no-repeat;
+    display: inline-block;
+    width: 260px;
+    height: 46px;
+    margin: 10px 0;
+    box-sizing: border-box;
+
+    &:hover {
+      border: 2px solid gray;
+      border-radius: 7px;
+    }
+  }
+  a:nth-child(2) {
+    background-image: url("https://s.zigbang.com/legacy/images/v2/main/btn_apple.png");
+  }
+  a:nth-child(3) {
+    background-image: url("https://s.zigbang.com/legacy/images/v2/main/btn_google.png");
+  }
+`;
+const Banner = styled.section`
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  h2 {
+    color: #49599a;
+    font-size: 16px;
+    margin: 20px;
+  }
+  button {
+    display: flex;
+    align-items: center;
+    align-self: center;
+    border-radius: 6px;
+    height: 24px;
+    background: none;
+    border: 1px solid #49599a;
+    outline: none;
+    color: #49599a;
+    padding: 5px 20px;
+  }
+`;
+const Footer = styled.footer`
+  height: 130px;
+  text-align: center;
+  background: rgb(246, 246, 246);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  ul {
+    margin: 15px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    a {
+      text-decoration: none;
+      color: #111;
+      padding: 0px 5px;
+      border-right: 1px solid gray;
+      &:hover {
+        text-decoration: underline;
+      }
+      li {
+        font-size: 13px;
+        vertical-align: baseline;
+        strong {
+          font-weight: bold;
+          text-decoration: underline;
+        }
+      }
+    }
+    a:last-child {
+      border: none;
+    }
+  }
+  h1 {
+    font-size: 24px;
+    margin-top: 15px;
+  }
+  h2 {
+    font-weight: bold;
+    margin-top: 15px;
   }
 `;
 
@@ -227,6 +351,45 @@ export default function Home() {
           </NewsList>
         </FeatureItem>
       </FeatureContainer>
+
+      <DownAppContainer>
+        <DownAppImg />
+        <DownAppLink>
+          <h1>직방앱을 다운받으세요!</h1>
+          <a href="/"></a>
+          <a href="/"></a>
+        </DownAppLink>
+      </DownAppContainer>
+      <Banner>
+        <h2>중개 사무소 가입 및 광고 방법이 궁금하신가요?</h2>
+        <button>자세히 알아보기&gt;</button>
+      </Banner>
+      <Footer>
+        <ul>
+          <a href="/">
+            <li>회사소개</li>
+          </a>
+          <a href="/">
+            <li>이용약관</li>
+          </a>
+          <a href="/">
+            <li>
+              <strong>개인정보 처리방침</strong>
+            </li>
+          </a>
+          <a href="/">
+            <li>위치기반 서비스 이용약관</li>
+          </a>
+          <a href="/">
+            <li>중개사 사이트 바로가기</li>
+          </a>
+        </ul>
+        <h1>
+          이 페이지는 <a href="https://www.zigbang.com/">zigbang.com</a>사이트를
+          클론코딩한 사이트입니다.
+        </h1>
+        <h2>유원근 (yhg0337@gmail.com)</h2>
+      </Footer>
     </div>
   );
 }
--- src/components/nav/MainHeader.js
+++ src/components/nav/MainHeader.js
@@ -1,5 +1,14 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const menuPop = keyframes`
+from {
+    opacity: 0
+  }
+  to {
+    opacity: 1
+  }
+`;
 
 const Header = styled.header`
   position: fixed;
@@ -11,6 +20,7 @@ const Header = styled.header`
   align-content: center;
   background: #fff;
   z-index: 10;
+  border-bottom: 1px solid #e5e5e5;
 `;
 const Logo = styled.a`
   background-image: url("https://s.zigbang.com/v1/web/common/new/h_logo_new.png");
@@ -19,6 +29,7 @@ const Logo = styled.a`
   width: 100px;
   height: 44px;
   margin: 10px;
+  cursor: pointer;
 `;
 const LeftMenu = styled.ul`
   flex: 1;
@@ -30,6 +41,15 @@ const LeftMenu = styled.ul`
     padding: 10px 20px;
     display: flex;
     flex-direction: column;
+    cursor: pointer;
+    &:hover {
+      ul {
+        display: initial;
+      }
+      span {
+        color: #7986cb;
+      }
+    }
     span {
       font-size: 15px;
       font-weight: bold;
@@ -39,9 +59,25 @@ const LeftMenu = styled.ul`
       font-size: 10px;
       color: #7986cb;
     }
+    ul {
+      animation: ${menuPop} 0.5s ease-in forwards;
+      padding-top: 23px;
+      display: none;
+      background: white;
+
+      li {
+        display: block;
+        font-size: 12px;
+        margin: 5px;
+        &:hover {
+          background: #7986cb;
+          color: #fff;
+          font-weight: bold;
+        }
+      }
+    }
   }
 `;
-
 const RightMenu = styled.div`
   display: flex;
   align-items: center;
@@ -55,6 +91,11 @@ const RightMenuBTN = styled.button`
   margin-right: 10px;
   color: #7986cb;
   background: #fff;
+  cursor: pointer;
+  &:hover {
+    background: #7986cb;
+    color: white;
+  }
 `;
 const RightMenuINFO = styled.div`
   display: flex;
@@ -80,23 +121,50 @@ export default function MainHeader() {
         <li>
           <span>아파트</span>
           <small>(매매/전월세/신축분양)</small>
+          <ul>
+            <li>매매/전.월세</li>
+            <li>신축분양</li>
+            <li>인구흐름</li>
+            <li>우리집 내놓기</li>
+          </ul>
         </li>
         <li>
           <span>빌라,투룸</span>
           <small>(매매/전월세)</small>
+          <ul>
+            <li>빌라,투룸 찾기</li>
+            <li>찜한 매물</li>
+            <li>빌라 내놓기(전월세만)</li>
+          </ul>
         </li>
-
         <li>
           <span>원룸</span>
           <small>(전/월세)</small>
+          <ul>
+            <li>방찾기</li>
+            <li>찜한 매물</li>
+            <li>방 내놓기(전월세만)</li>
+          </ul>
         </li>
         <li>
           <span>오피스텔/도시형생활주택</span>
           <small>(전/월세)</small>
+          <ul>
+            <li>오피스텔찾기</li>
+            <li>찜한 매물</li>
+            <li>오피스텔 내놓기(전월세만)</li>
+          </ul>
         </li>
         <li>
           <span>창업/사무실</span>
           <small>(임대/매매)</small>
+          <ul>
+            <li>상가.점포</li>
+            <li>사무실</li>
+            <li>공유오피스</li>
+            <li>찜한 매물</li>
+            <li>상가/사무실 내놓기</li>
+          </ul>
         </li>
       </LeftMenu>
       <RightMenu>
